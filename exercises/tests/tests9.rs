edaf6a886131568b887f38d2eb535d702d01063a
xBinJ <daixi1703163@163.com>
2025-03-27 08:46:23 +0800

    finish tests exercises.

--- exercises/tests/tests9.rs
+++ exercises/tests/tests9.rs
@@ -34,9 +34,9 @@ extern "Rust" {
     fn my_demo_function_alias(a: u32) -> u32;
 }
 
-mod Foo {
+mod foo {
     // No `extern` equals `extern "Rust"`.
-    fn my_demo_function(a: u32) -> u32 {
+    pub fn my_demo_function(a: u32) -> u32 {
         a
     }
 }
@@ -44,6 +44,8 @@ mod Foo {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use foo::my_demo_function;
+    use foo::my_demo_function as my_demo_function_alias;
 
     #[test]
     fn test_success() {
